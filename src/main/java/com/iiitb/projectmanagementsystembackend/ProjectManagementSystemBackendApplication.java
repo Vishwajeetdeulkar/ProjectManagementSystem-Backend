d6700e4dbc52a5037afb5e5ae140d8499064ed14
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-04-19 18:41:36 +0530

    Initial commit : Bare project vcs setup

---
+++ src/main/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.projectmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.projectmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
