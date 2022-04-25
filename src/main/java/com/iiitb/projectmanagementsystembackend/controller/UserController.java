2d98176ef6894f96e8aa028447328453122fe4d4
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-04-25 14:49:55 +0530

    JWT login functionality added

--- src/main/java/com/iiitb/projectmanagementsystembackend/controller/UserController.java
+++ src/main/java/com/iiitb/projectmanagementsystembackend/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -67,4 +66,3 @@ public class UserController {
     }
 
 }
-
