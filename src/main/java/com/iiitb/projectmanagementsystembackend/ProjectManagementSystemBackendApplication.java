a6550e36fca18abcb7a1106f92a8f255fe47c606
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-04-28 21:44:53 +0530

    application.properties updated

--- src/main/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplication.java
+++ src/main/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplication.java
@@ -1,10 +1,12 @@
 package com.iiitb.projectmanagementsystembackend;
 
+import com.iiitb.projectmanagementsystembackend.data.model.Project;
 import com.iiitb.projectmanagementsystembackend.data.model.Role;
 import com.iiitb.projectmanagementsystembackend.data.model.User;
 import com.iiitb.projectmanagementsystembackend.data.model.UserDto;
 import com.iiitb.projectmanagementsystembackend.data.repository.RoleDao;
 import com.iiitb.projectmanagementsystembackend.data.repository.UserDao;
+import com.iiitb.projectmanagementsystembackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectManagementSystemBackendApplication implements CommandLineRunner {
-
+   // @Autowired
+    //UserDao ob;
+    //private UserService ob;
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementSystemBackendApplication.class, args);
     }
@@ -21,6 +25,8 @@ public class ProjectManagementSystemBackendApplication implements CommandLineRun
 
     @Override
     public void run(String... args) throws Exception {
+//       UserDto first = new UserDto("m1", "m", "m1@manager.org", "1234567789", "1234567789", "manager");
+//       ob.save(first);
 
     }
 }
