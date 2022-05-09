5346e6061635668a52d652f84d77f9d5fcba6207
Vishwajeetdeulkar <vishwajeetd13@gmail.com>
2022-05-09 17:32:13 +0530

    ansible settings for deployment added

--- src/main/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplication.java
+++ src/main/java/com/iiitb/projectmanagementsystembackend/ProjectManagementSystemBackendApplication.java
@@ -1,17 +1,17 @@
 package com.iiitb.projectmanagementsystembackend;
 
 import com.iiitb.projectmanagementsystembackend.controller.UserController;
-import com.iiitb.projectmanagementsystembackend.data.model.Project;
-import com.iiitb.projectmanagementsystembackend.data.model.Role;
-import com.iiitb.projectmanagementsystembackend.data.model.User;
 import com.iiitb.projectmanagementsystembackend.data.model.UserDto;
-import com.iiitb.projectmanagementsystembackend.data.repository.RoleDao;
-import com.iiitb.projectmanagementsystembackend.data.repository.UserDao;
 import com.iiitb.projectmanagementsystembackend.service.UserService;
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -23,6 +23,8 @@ public class ProjectManagementSystemBackendApplication implements CommandLineRun
    @Autowired
    private UserService userService;
     UserController ob = new UserController();
+    @Value("${http.port}")
+    private int httpPort;
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementSystemBackendApplication.class, args);
     }
@@ -33,5 +35,20 @@ public class ProjectManagementSystemBackendApplication implements CommandLineRun
          UserDto first = new UserDto("admin", "admin", "admin@admin.org", "9960429909", "Neha Kothari", "admin");
         userService.save(first);
 
+    }
+
+    // Let's configure additional connector to enable support for both HTTP and HTTPS
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
     }
 }
